a73dd4d5693983215cb04b92d5307b561a22ce2b
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-01-15 03:55:38 -0500

    v1

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const server = express();
+const mongoose = require("mongoose");
+const error_handler = require("node-error-handler");
+
+
+
+
+server.use(express.json());
+server.use(cors());
+
+
+server.use(error_handler({ log: true, debug: true }));
+
+const PORT = process.env.PORT || 3001;
+
+//connect to database
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => server.listen(PORT, () => console.log("connected to", PORT)));
