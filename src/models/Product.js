969f6d36ef9ef01f3b52bb3dc391f1223bbae209
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-01-15 04:17:40 -0500

    model created

---
+++ src/models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const schema = mongoose.schema;
+
+const ProductSchema = new schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  imageUrl: String,
+  price: Number,
+  category: String,
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model(ProductSchema, "products");
